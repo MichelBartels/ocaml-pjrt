46037d86f5fb8317b96e9d0a2fc323a04a04b3a6
Michel Bartels <login@michelbartels.com>
2024-11-15 17:21:42 +0000

    add matmul

--- lib/dsl.ml
+++ lib/dsl.ml
@@ -2,6 +2,39 @@ let fn = Ir.create_func
 
 module Var = Ir.Var
 
+let matmul a b =
+  let a_shape = Ir.shape_of_var a in
+  let b_shape = Ir.shape_of_var b in
+  let rec prefix = function
+    | [_; _] ->
+        []
+    | x :: xs ->
+        x :: prefix xs
+    | [] ->
+        failwith "matmul requires at least 2 dims"
+  in
+  let rec common_prefix a b =
+    match (a, b) with
+    | [], _ | _, [] ->
+        []
+    | a :: a_tl, b :: b_tl when a = b ->
+        a :: common_prefix a_tl b_tl
+    | _ ->
+        []
+  in
+  let batching_dims =
+    List.init
+      (List.length (common_prefix (prefix a_shape) (prefix b_shape)))
+      Fun.id
+  in
+  Ir.Var.DotProduct
+    ( a
+    , b
+    , [List.length a_shape - 1]
+    , [List.length b_shape - 2]
+    , batching_dims
+    , batching_dims )
+
 let ( + ) a b = Var.Add (a, b)
 
 let ( - ) a b = Var.Subtract (a, b)
@@ -33,25 +66,26 @@ let full_i1 value = full (I1 value)
 let full_like value var =
   Ir.shape_of_var var |> Tensor.full value |> Tensor.to_ir
 
-let ones_like : type a. a Ir.tensor Ir.Var.t -> a Ir.tensor Ir.Var.t =
- fun var ->
-  match Ir.ValueType.of_var var with
+let ones : type a. a Ir.tensor Ir.ValueType.t -> a Ir.tensor Ir.Var.t = function
   | Tensor_type (shape, F32) ->
       full (F32 1.) shape
   | Tensor_type (shape, I1) ->
-      full (I1 true) shape
+      full (I1 false) shape
   | Tensor_type (shape, I64) ->
       full (I64 1) shape
 
-let zeros_like : type a. a Ir.tensor Ir.Var.t -> a Ir.tensor Ir.Var.t =
- fun var ->
-  match Ir.ValueType.of_var var with
+let ones_like t = ones (Ir.ValueType.of_var t)
+
+let zeros : type a. a Ir.tensor Ir.ValueType.t -> a Ir.tensor Ir.Var.t =
+  function
   | Tensor_type (shape, F32) ->
       full (F32 0.) shape
   | Tensor_type (shape, I1) ->
       full (I1 false) shape
   | Tensor_type (shape, I64) ->
       full (I64 0) shape
+
+let zeros_like t = zeros (Ir.ValueType.of_var t)
 
 let norm mean stddev shape =
   Ir.tag
