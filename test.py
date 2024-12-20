84b153265bf020a3ec3353017c3c7f59335f70b8
Michel Bartels <login@michelbartels.com>
2024-12-20 13:45:55 +0000

    update test.py

--- test.py
+++ test.py
@@ -4,33 +4,44 @@ from mnist import mnist
 from tqdm import tqdm
 from matplotlib import pyplot as plt
 
-train = ireert.load_vm_flatbuffer_file("train.vmfb", driver="local-task").fn320
-decoder = ireert.load_vm_flatbuffer_file("decoder.vmfb", driver="local-task").fn1197
+train = ireert.load_vm_flatbuffer_file("train.vmfb", driver="local-task").fn3137
+reconstruct = ireert.load_vm_flatbuffer_file("reconstruct.vmfb", driver="local-task").fn7724
 
-w1_encoder = np.random.normal(size=(784, 512)).astype(np.float32) * 0.01
-b1_encoder = np.zeros((512,), dtype=np.float32)
-w2_encoder = np.random.normal(size=(512, 16)).astype(np.float32) * 0.01
-b2_encoder = np.zeros((16,), dtype=np.float32)
-w3_encoder = np.random.normal(size=(512, 16)).astype(np.float32) * 0.01
-b3_encoder = np.zeros((16,), dtype=np.float32)
 
-w1_decoder = np.random.normal(size=(16, 512)).astype(np.float32) * 0.01
-b1_decoder = np.zeros((512,), dtype=np.float32)
-w2_decoder = np.random.normal(size=(512, 784)).astype(np.float32) * 0.01
-b2_decoder = np.zeros((784,), dtype=np.float32)
+def init(shape):
+    return (np.zeros(shape=shape).astype(np.float32),
+     np.ones(shape=shape).astype(np.float32) * 0.001)
 
-params = [w1_encoder, b1_encoder, w2_encoder, b2_encoder, w3_encoder, b3_encoder, w1_decoder, b1_decoder, w2_decoder, b2_decoder]
+def init_dense(input, output):
+    return lambda: [*init((input, output)), *init((1, output))]
+
+encoder_1 = init_dense(784, 512)
+encoder_2 = init_dense(512, 16)
+encoder_3 = init_dense(512, 16)
+
+decoder_1 = init_dense(16, 512)
+decoder_2 = init_dense(512, 784)
+
+param_initialisers = [encoder_1, encoder_2, encoder_3, decoder_1, decoder_2]
+
+params = []
+
+for i in range(3):
+    for init_fn in param_initialisers:
+        params.extend(init_fn())
+    if i == 0:
+        params.append(np.array(1.0).astype(np.float32))
 
 num_epochs = 25000
 
 bar = tqdm(zip(range(num_epochs), mnist()), total=num_epochs)
 
 for i, (x, _) in bar:
-    [*params, loss] = [x for x in train(*params, x)]
+    x = np.expand_dims(x, 1)
+    [*params, loss] = [x for x in train(x, *params)]
     bar.set_description(f"Loss: {loss.to_host()}")
-    if (i + 1) % 2500 == 0:
-        x = np.random.normal(size=(512, 16)).astype(np.float32)
-        y_pred = decoder(*params[6:], x).to_host()[0]
+    if (i) % 2500 == 0:
+        y_pred = reconstruct(x, *params[:20]).to_host()[0]
         y_pred = y_pred.reshape(28, 28)
         plt.imshow(y_pred, cmap="gray")
         plt.show()
--- test_jax.py
+++ test_jax.py
@@ -1,22 +1,26 @@
 import jax.numpy as jnp
-from jax import value_and_grad, jit
+from jax import value_and_grad, jit, random
 import numpy as np
-from mnist import mnist
+from mnist import mnist, batch_size
 from tqdm import tqdm
 from matplotlib import pyplot as plt
 from optax import adamw, sgd, apply_updates
 
-w1_encoder = np.random.normal(size=(784, 512)).astype(np.float32) * 0.01
-b1_encoder = np.zeros((512,), dtype=np.float32)
-w2_encoder = np.random.normal(size=(512, 16)).astype(np.float32) * 0.01
-b2_encoder = np.zeros((16,), dtype=np.float32)
-w3_encoder = np.random.normal(size=(512, 16)).astype(np.float32) * 0.01
-b3_encoder = np.zeros((16,), dtype=np.float32)
+def init(shape):
+    return (np.zeros(shape=shape).astype(np.float32),
+     np.ones(shape=shape).astype(np.float32) * 0.001)
 
-w1_decoder = np.random.normal(size=(16, 512)).astype(np.float32) * 0.01
-b1_decoder = np.zeros((512,), dtype=np.float32)
-w2_decoder = np.random.normal(size=(512, 784)).astype(np.float32) * 0.01
-b2_decoder = np.zeros((784,), dtype=np.float32)
+w1_encoder = init((784, 512))
+b1_encoder = init((512,))
+w2_encoder = init((512, 16))
+b2_encoder = init((16,))
+w3_encoder = init((512, 16))
+b3_encoder = init((16,))
+
+w1_decoder = init((16, 512))
+b1_decoder = init((512,))
+w2_decoder = init((512, 784))
+b2_decoder = init((784,))
 
 def sigmoid(x):
     return 1 / (1 + jnp.exp(-x))
@@ -27,48 +31,66 @@ def relu(x):
 def tanh(x):
     return jnp.tanh(x)
 
-def encoder(x, w1_encoder, b1_encoder, w2_encoder, b2_encoder, w3_encoder, b3_encoder):
-    hidden = tanh(jnp.dot(x, w1_encoder) + b1_encoder)
-    mean = jnp.dot(hidden, w2_encoder) + b2_encoder
-    log_var = jnp.dot(hidden, w3_encoder) + b3_encoder
-    return mean, log_var
+def reparameterize(seed, mean, std, add_batch=True):
+    key_1, key_2 = random.split(seed)
+    eps = random.normal(key_1, [batch_size, *mean.shape] if add_batch else mean.shape)
+    return key_2, mean + std * eps
 
-def reparameterize(mean, log_var):
-    std = jnp.exp(log_var)
-    eps = np.random.normal(size=std.shape).astype(np.float32)
-    return mean + std * eps
+def encoder(seed, x, w1_encoder, b1_encoder, w2_encoder, b2_encoder, w3_encoder, b3_encoder):
+    seed, w1_encoder = reparameterize(seed, *w1_encoder)
+    seed, b1_encoder = reparameterize(seed, *b1_encoder)
+    hidden = tanh(jnp.matmul(jnp.expand_dims(x, 1), w1_encoder) + jnp.expand_dims(b1_encoder, 1))
+    seed, w2_encoder = reparameterize(seed, *w2_encoder)
+    seed, b2_encoder = reparameterize(seed, *b2_encoder)
+    mean = jnp.matmul(hidden, w2_encoder) + jnp.expand_dims(b2_encoder, 1)
+    seed, w3_encoder = reparameterize(seed, *w3_encoder)
+    seed, b3_encoder = reparameterize(seed, *b3_encoder)
+    log_var = jnp.matmul(hidden, w3_encoder) + jnp.expand_dims(b3_encoder, 1)
+    return seed, mean, log_var
 
-def decoder(z, w1_decoder, b1_decoder, w2_decoder, b2_decoder):
-    hidden = tanh(jnp.dot(z, w1_decoder) + b1_decoder)
-    mean = sigmoid(jnp.dot(hidden, w2_decoder) + b2_decoder)
-    return mean
+def decoder(seed, z, w1_decoder, b1_decoder, w2_decoder, b2_decoder):
+    seed, w1_decoder = reparameterize(seed, *w1_decoder)
+    seed, b1_decoder = reparameterize(seed, *b1_decoder)
+    hidden = tanh(jnp.matmul(z, w1_decoder) + jnp.expand_dims(b1_decoder, 1))
+    seed, w2_decoder = reparameterize(seed, *w2_decoder)
+    seed, b2_decoder = reparameterize(seed, *b2_decoder)
+    mean = sigmoid(jnp.matmul(hidden, w2_decoder) + jnp.expand_dims(b2_decoder, 1))
+    return seed, mean
 
 def mse(y_true, y_pred):
     return jnp.sum(jnp.square(y_true - y_pred), -1)
 
-def kl(mean, log_var):
-    return -0.5 * jnp.sum(1 + log_var - jnp.square(mean) - jnp.exp(log_var), -1)
+def kl(mean, var, log_var):
+    return -0.5 * jnp.sum(1 + log_var - jnp.square(mean) - var, -1)
 
-def f(w1_encoder, b1_encoder, w2_encoder, b2_encoder, w3_encoder, b3_encoder, w1_decoder, b1_decoder, w2_decoder, b2_decoder, x):
-    mean, log_var = encoder(x, w1_encoder, b1_encoder, w2_encoder, b2_encoder, w3_encoder, b3_encoder)
-    z = reparameterize(mean, log_var)
-    y_pred = decoder(z, w1_decoder, b1_decoder, w2_decoder, b2_decoder)
-    loss = mse(x, y_pred) + kl(mean, log_var)
-    return jnp.mean(loss)
+def kl_param(param):
+    mean, var = param
+    var *= 1000
+    return kl(mean, var, jnp.log(var))
 
-lr = 0.02
+def f(seed, w1_encoder, b1_encoder, w2_encoder, b2_encoder, w3_encoder, b3_encoder, w1_decoder, b1_decoder, w2_decoder, b2_decoder, x):
+    seed, mean, log_var = encoder(seed, x, w1_encoder, b1_encoder, w2_encoder, b2_encoder, w3_encoder, b3_encoder)
+    seed, z = reparameterize(seed, mean, jnp.exp(log_var), add_batch=False)
+    seed, y_pred = decoder(seed, z, w1_decoder, b1_decoder, w2_decoder, b2_decoder)
+    mse_ = jnp.mean(mse(jnp.expand_dims(x, 1), y_pred))
+    kl_ = jnp.mean(kl(mean, jnp.exp(log_var), log_var))
+    kl_params = jnp.mean(kl_param(w1_encoder)) + jnp.mean(kl_param(b1_encoder)) + jnp.mean(kl_param(w2_encoder)) + jnp.mean(kl_param(b2_encoder)) + jnp.mean(kl_param(w3_encoder)) + jnp.mean(kl_param(b3_encoder)) + jnp.mean(kl_param(w1_decoder)) + jnp.mean(kl_param(b1_decoder)) + jnp.mean(kl_param(w2_decoder)) + jnp.mean(kl_param(b2_decoder))
+    loss = mse_ + kl_ + kl_params
+    return loss
+
+lr = 0.0001
 
 params = [w1_encoder, b1_encoder, w2_encoder, b2_encoder, w3_encoder, b3_encoder, w1_decoder, b1_decoder, w2_decoder, b2_decoder]
 
-opt = sgd(learning_rate=lr)
+opt = adamw(learning_rate=lr)
 opt_state = opt.init(tuple(params))
 
 params = [*params, opt_state]
 
 @jit
-def optim(params, x):
+def optim(seed, params, x):
     [*params, opt_state] = params
-    v, grad = value_and_grad(f, argnums=range(len(params)))(*params, x)
+    v, grad = value_and_grad(f, argnums=range(1, len(params) + 1))(seed, *params, x)
     updates, opt_state = opt.update(grad, opt_state, tuple(params))
     params = apply_updates(tuple(params), updates)
     return [*params, opt_state], v
@@ -79,11 +101,15 @@ num_steps = 25000
 bar = tqdm(zip(range(num_steps), mnist()), total=num_steps)
 
 for i, (x, _) in bar:
-    params, loss = optim(params, x)
+    seed = random.key(i)
+    if i == 0:
+        print(optim.lower(seed, params, x).as_text())
+    params, loss = optim(seed, params, x)
     bar.set_description(f"Loss: {loss}")
     if (i + 1) % 2500 == 0:
-        y_pred = decoder(encoder(x[0], *params[:6])[0], *params[6:10])
+        seed, mean, _ = encoder(seed, x, *params[:6])
+        _, y_pred = decoder(seed, mean, *params[6:10])
         plt.imshow(x[0].reshape(28, 28), cmap='gray')
         plt.show()
-        plt.imshow(y_pred.reshape(28, 28), cmap='gray')
+        plt.imshow(y_pred[0].reshape(28, 28), cmap='gray')
         plt.show()
