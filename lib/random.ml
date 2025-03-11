90a39fbad9578fdfa1d3ec682542768f1a3dfaea
Michel Bartels <login@michelbartels.com>
2025-03-11 11:36:16 +0000

    wrong erfinv

--- lib/random.ml
+++ lib/random.ml
@@ -34,18 +34,29 @@ let uniform_f32 ?(key = key) shape =
   let x = reshape shape x in
   no_grad x
 
+let erfinv x =
+  let sign = select (x <.> 0.0) (full_like F32 (-1.0) x) (full_like F32 1. x) in
+  let x = (1.0 -.< x) *@ (1.0 +.< x) in
+  let x = ln x in
+  let tt1 = (2.0 /. (Float.pi *. 0.147)) +.< (0.5 *.< x) in
+  let tt2 = 1.0 /. 0.147 *.< x in
+  (sign *@ tt1 *@ tt1) -@ tt2
+
+(* let normal_f32 ?(key = key) shape = *)
+(*   let size = List.fold_left ( * ) 1 shape in *)
+(*   let half_size = size / 2 in *)
+(*   assert (size mod 2 = 0) ; *)
+(*   let flat_shape = [half_size] in *)
+(*   let u0 = uniform_f32 ~key flat_shape in *)
+(*   let u1 = uniform_f32 ~key flat_shape in *)
+(*   let factor = sqrt (-2.0 *.< ln (1. -.< u0)) in *)
+(*   let inner = 2.0 *. Float.pi *.< u1 in *)
+(*   let z_0 = factor *@ sin inner in *)
+(*   let z_1 = factor *@ cos inner in *)
+(*   concat 0 [z_0; z_1] |> reshape shape |> no_grad *)
+
 let normal_f32 ?(key = key) shape =
-  let size = List.fold_left ( * ) 1 shape in
-  let half_size = size / 2 in
-  assert (size mod 2 = 0) ;
-  let flat_shape = [half_size] in
-  let u0 = uniform_f32 ~key flat_shape in
-  let u1 = uniform_f32 ~key flat_shape in
-  let factor = sqrt (-2.0 *.< ln (1. -.< u0)) in
-  let inner = 2.0 *. Float.pi *.< u1 in
-  let z_0 = factor *@ sin inner in
-  let z_1 = factor *@ cos inner in
-  concat 0 [z_0; z_1] |> reshape shape |> no_grad
+  Float.sqrt 2. *.< erfinv ((uniform_f32 ~key shape *.> 2.0) -.> 1.0)
 
 let current_seed () = Effect.perform (Effects.Counter 0)
 
