9bbab269cec0ca6885090c733541a54d6e44326a
Michel Bartels <kontakt@michelbartels.com>
2025-03-11 15:21:16 +0100

    rename stddev to var

--- lib/distribution.ml
+++ lib/distribution.ml
@@ -4,23 +4,35 @@ type t =
   | Normal of (Ir.Tensor.f32, float) Ir.Var.u * (Ir.Tensor.f32, float) Ir.Var.u
   | Uniform of (Ir.Tensor.f32, float) Ir.Var.u * (Ir.Tensor.f32, float) Ir.Var.u
 
-let sample = function
+let sample dist batch_size = match dist with
   | Normal (mean, std) ->
+      let (mean, std) = (match batch_size with
+        | Some batch_size ->
+            let mean = Ir.Var.BroadcastInDim (mean, [batch_size]) in
+            let std = Ir.Var.BroadcastInDim (std, [batch_size]) in
+            (mean, std)
+        | None -> (mean, std)) in
       let shape = Ir.shape_of_var mean in
       let sample = norm ~.0. ~.1. shape in
       (sample *@ std) +@ mean
   | Uniform (low, high) ->
+      let (low, high) = (match batch_size with
+        | Some batch_size ->
+            let low = Ir.Var.BroadcastInDim (low, [batch_size]) in
+            let high = Ir.Var.BroadcastInDim (high, [batch_size]) in
+            (low, high)
+        | None -> (low, high)) in
       let shape = Ir.shape_of_var low in
       let sample = uniform ~.0. ~.1. shape in
       (sample *@ (high -@ low)) +@ low
 
 let log_prob dist x =
   match dist with
-  | Normal (mean, var) ->
+  | Normal (mean, stddev) ->
       let shape = Ir.shape_of_var mean in
       let axes = List.mapi (fun i _ -> i) shape in
-      let squared_error = (x -@ mean) /@ var *@ ((x -@ mean) /@ var) /.> 2. in
-      let regulariser = (Float.log (2. *. Float.pi) /. 2.) +.< ln var in
+      let squared_error = (x -@ mean) /@ stddev *@ ((x -@ mean) /@ stddev) /.> 2. in
+      let regulariser = (Float.log (2. *. Float.pi) /. 2.) +.< ln stddev in
       Dsl.sum axes @@ ~-@(squared_error +@ regulariser)
   | Uniform (low, high) ->
       let shape = Ir.shape_of_var low in
--- lib/effects.ml
+++ lib/effects.ml
@@ -1,7 +1,7 @@
 type _ Effect.t +=
   | Counter : int -> (Ir.Tensor.u64, Unsigned.uint64) Ir.Var.u Effect.t
   | Sample :
-      Distribution.t * Distribution.t
+      Distribution.t * Distribution.t * int option
       -> (Ir.Tensor.f32, float) Ir.Var.u Effect.t
 
 let dummy_handler f =
@@ -14,9 +14,9 @@ let dummy_handler f =
               Some
                 (fun (k : (a, _) continuation) ->
                   continue k @@ Dsl.scalar_u64 "0" )
-          | Sample (_, d) ->
+          | Sample (_, d, batch_size) ->
               Some
                 (fun (k : (a, _) continuation) ->
-                  continue k @@ Distribution.sample d )
+                  continue k @@ Distribution.sample d batch_size )
           | _ ->
               None ) }
